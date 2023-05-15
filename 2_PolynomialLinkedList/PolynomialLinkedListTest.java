8c64fe03284e605822dfac86a7ca0bf535d6cd7e
도형준 <105966480+dorae222@users.noreply.github.com>
2023-05-15 14:13:12 +0900

    PolynomialLinkedList

---
+++ 2_PolynomialLinkedList/PolynomialLinkedListTest.java
@@ -0,0 +1,73 @@
+package org.likebnb.ds.apps;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PolynomialLinkedListTest {
+	final static private Logger LOG = LoggerFactory.getLogger(PolynomialLinkedListTest.class);	
+	
+	static public PolynomialLinkedList p1;
+	static public PolynomialLinkedList p2;
+	static public PolynomialLinkedList polyAddition;
+	
+	@BeforeAll
+	static public void beforeAll() {
+		p1 = new PolynomialLinkedList();
+		p2 = new PolynomialLinkedList();
+		polyAddition = new PolynomialLinkedList();
+	}
+	
+	@AfterAll
+	static public void afterAll() {
+		p1 = null;
+		p2 = null;
+		polyAddition = null;
+	}
+	
+	@Test
+	public void tesrCase01() {
+		TermNode node = new TermNode(3, 4);
+		p1.addTerm(node);
+		p1.addTerm(new TermNode(7, 0));
+		p1.addTerm(new TermNode(3, 2));
+		p1.addTerm(new TermNode(9, 3));
+		p1.removeTerm(1);
+		p1.removeTerm(3);
+		LOG.info("Final Poly: {}", p1.printPoly());
+	}
+	
+	// TODO: 1) 다항식, p2를 2X^4 + 5X^3 + 12X + 3로 만들고
+	// TODO: 2) 다항식, p1 + p2를 실행하는 테스트 케이스를 코딩하고 실행해보자. 
+	// --> 이를 위해선 testCase02()의 이름을 먼저 testCase03()으로 바꿔야 한다.
+	
+	@Test
+	public void testCase02() {
+		polyAddition.addPoly(p1, p2);
+
+		LOG.info("    p1 --> {}", p1.printPoly());
+		LOG.info("    p2 --> {}", p2.printPoly());
+		LOG.info(" p1+p2 --> {}", polyAddition.printPoly());
+	}
+
+    @Test
+    public void testCase03() {
+        p2 = new PolynomialLinkedList()
+        LOG.info("p2 초기값{}", p2.printPoly());
+
+        p2.addTerm(new TermNode(2, 4)); // 2x+4
+        LOG.info("p2 2X^4: {}", p2.printPoly());
+    }
+
+    @TEST
+    public void testCase04() { // 과제: p1, p2 addPoly testCase04()로 제출     LOG.info("학과,학번,성명"); 출력까지
+        p2 = new PolynomialLinkedList()
+        LOG.info("p2 초기값{}", p2.printPoly());
+
+        p2.addTerm(new TermNode(2, 4)); // 2x+4
+        LOG.info("p2 2X^4: {}", p2.printPoly());
+    }
+
+}
